332206251cc8b021bb9ca15985c5632d8697366e
oh-facts <as17793@nyu.edu>
2023-07-08 13:59:10 +0530

    should fix netlify refreshing issue

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
 
         <Routes>
           <Route index element={<Home />} />
-          <Route path="/home" element={<Home />} />
+          <Route path="" element={<Home />} />
 
           <Route path="/about" element={<About />} />
 
--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -2,13 +2,13 @@ import { useNavigate } from "react-router-dom";
 const MenuBar = () => {
    
     const tabs = [
-        {title: 'Home', id: 1},
-        {title: 'About', id: 2},
-        {title: 'Media', id: 3},
-        {title: 'Team', id: 4},
-        {title: 'Blog',id: 5},
-        {title: 'FAQ', id: 6},
-        {title: 'Contact', id: 7}
+        {title: 'Home', id: 1, path:''},
+        {title: 'About', id: 2, path:'/about'},
+        {title: 'Media', id: 3, path:'/media'},
+        {title: 'Team', id: 4, path:'/team'},
+        {title: 'Blog',id: 5, path:'/blog'},
+        {title: 'FAQ', id: 6, path:'/faq'},
+        {title: 'Contact', id: 7, path:'/contact'}
         
     ]
    const navigate = useNavigate()
@@ -16,7 +16,7 @@ const MenuBar = () => {
         tabs.map((tab) =>(
         <>
         
-            <button key = {tab.id} onClick={() =>navigate(tab.title)}>
+            <button key = {tab.id} onClick={() =>navigate(tab.path)}>
                 {tab.title}    
             </button>
         </>
