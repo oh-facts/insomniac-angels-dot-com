162722cd8e0e51512bbbf89d5a5628d314c2a79c
oh-facts <as17793@nyu.edu>
2023-07-17 13:29:15 +0530

    sub-menus collapse after clicking

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,35 +1,50 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-const SubMenu = ({ items }) => {
+const SubMenuItem = ({ item, onCloseSubMenu }) => {
+  const handleClick = () => {
+    // Call the onCloseSubMenu function to close the sub-menu
+    onCloseSubMenu();
+  };
+
+  return (
+    <li key={item.id}>
+      {item.path.startsWith('http') ? (
+        <a href={item.path} className="sub-menu-link" target="_blank" rel="noopener noreferrer" onClick={handleClick}>
+          {item.title}
+        </a>
+      ) : (
+        <Link to={item.path} className="sub-menu-link" onClick={handleClick}>
+          {item.title}
+        </Link>
+      )}
+    </li>
+  );
+};
+
+const SubMenu = ({ items, onCloseSubMenu }) => {
   if (items.length === 0) {
     return null;
   }
+
   return (
     <ul className="sub-menu">
       {items.map((item) => (
-        <li key={item.id}>
-          {item.path.startsWith('http') ? (
-            <a href={item.path} className="sub-menu-link" target="_blank" rel="noopener noreferrer">
-              {item.title}
-            </a>
-          ) : (
-            <Link to={item.path} className="sub-menu-link">
-              {item.title}
-            </Link>
-          )}
-        </li>
+        <SubMenuItem key={item.id} item={item} onCloseSubMenu={onCloseSubMenu} />
       ))}
     </ul>
   );
 };
-
 
 const MenuItem = ({ title, path, subMenuItems }) => {
   const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
 
   const toggleSubMenu = () => {
     setIsSubMenuOpen(!isSubMenuOpen);
+  };
+
+  const closeSubMenu = () => {
+    setIsSubMenuOpen(false);
   };
 
   return (
@@ -39,52 +54,45 @@ const MenuItem = ({ title, path, subMenuItems }) => {
       onMouseLeave={toggleSubMenu}
     >
       <Link to={path} className="menu-link">
-      {title}
+        {title}
       </Link>
-      {isSubMenuOpen && <SubMenu items={subMenuItems} />}
+      {isSubMenuOpen && <SubMenu items={subMenuItems} onCloseSubMenu={closeSubMenu} />}
     </div>
   );
 };
 
 const MenuBar = () => {
   const tabs = [
-    { title: "Home", id: 1, path: "" },
+    { title: 'Home', id: 1, path: '' },
     {
-      title: "About",
+      title: 'About',
       id: 2,
       subMenuItems: [
-        { title: "Setting", id: 21, path: "/about/setting" },
-        { title: "Characters", id: 22, path: "/about/characters" },
-        
+        { title: 'Setting', id: 21, path: '/about/setting' },
+        { title: 'Characters', id: 22, path: '/about/characters' },
       ],
     },
-    { title: "Media", id: 3, path: "/media" },
-    
+    { title: 'Media', id: 3, path: '/media' },
     {
-      title: "Info",
+      title: 'Info',
       id: 5,
       subMenuItems: [
-        { title: "Team", id: 51, path: "/info/team" },
-        { title: "YouTube \u2197", id: 52, path: "https://www.youtube.com/channel/UCfYuZbToCun8OkYJ5K37Nig" },
-        { title: "com", id: 53, path: "/info/community" },
-        
-        { title: "FAQ", id: 54, path: "/info/faq" }
-      ]
+        { title: 'Team', id: 51, path: '/info/team' },
+        { title: 'YouTube \u2197', id: 52, path: 'https://www.youtube.com/channel/UCfYuZbToCun8OkYJ5K37Nig' },
+        { title: 'com', id: 53, path: '/info/community' },
+        { title: 'FAQ', id: 54, path: '/info/faq' },
+      ],
     },
   ];
 
   return (
     <div className="menu-bar">
       {tabs.map((tab) => (
-        <MenuItem
-          key={tab.id}
-          title={tab.title}
-          path={tab.path}
-          subMenuItems={tab.subMenuItems || []}
-        />
+        <MenuItem key={tab.id} title={tab.title} path={tab.path} subMenuItems={tab.subMenuItems || []} />
       ))}
     </div>
   );
 };
 
 export default MenuBar;
+ 
