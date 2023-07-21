76bc381ca7200184d0ae42c2d3b5e1d7c2cb30e4
oh-facts <as17793@nyu.edu>
2023-07-21 17:08:01 +0530

    Home page content

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,17 +1,47 @@
+import scene1 from '../assets/homePage/scene1.png'
 import earth1 from '../assets/earth1.jpg'
 
 const About = () => {
 
   return (
     <div className='card-container'>
-      <span style={{ fontSize: '2vw', textAlign: 'center', color: ' #ff9324' }}> A Godless World </span>
+
+
       <div className='card'>
-        <img src={earth1} alt='banner' />
+       
+        <img src={scene1} alt='scene1' style={{width:"40%", paddingTop:"7%"}} />
 
         <div className="text-box">
 
           <p>
-            <span style={{ fontSize: '0.9vw', textAlign: 'left' }}>
+            <span style={{ fontSize: '1.25rem', textAlign: 'left' }}>
+             What are 10 things you'd like to do before you die, you think to yourself as you see Mors Code on the NEWS, 
+             level a few districts, kill a million something people and rip the president into two and refer to them as 
+             the Presidents of Yinnewa.  
+             <br />
+             <br />
+             Its been a few hours since Mors Code started, and things haven't been looking very nice. Even the angels have stopped showing up since 
+             they have little intention of being ripped up.
+             <br />
+             <br />
+             (ALF)IA is a first person story driven hack and slasher
+            </span>
+          </p>
+
+        </div>
+      </div>
+
+
+
+
+      <div className='card'>
+        <span style={{ fontSize: '2vw', textAlign: 'center', color: ' #ff9324' }}> A Godless World </span>
+        <img src={earth1} alt='banner' style={{width:"25%", paddingTop:"5%"}} />
+
+        <div className="text-box">
+
+          <p>
+            <span style={{ fontSize: '1.25rem', textAlign: 'left' }}>
               Welcome to Yinneva, where the people are tired, the devils are in excess, and the Gods are quite dead. Thankfully,
               some angels still exist and make genuine attempts at preserving the human race. God bless Yinneva.
             </span>
@@ -19,6 +49,7 @@ const About = () => {
 
         </div>
       </div>
+
     </div>
 
   );
