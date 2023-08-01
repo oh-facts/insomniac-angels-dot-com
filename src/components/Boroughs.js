2ebb41409d74ea5013865c841cced4a7a80b3dcd
oh-facts <as17793@nyu.edu>
2023-08-01 09:29:18 +0530

    art/content for boroughs

--- src/components/Boroughs.js
+++ src/components/Boroughs.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react';
 import '../styles/components/Boroughs.css';
 import city1 from '../assets/boroughs/city1.png'
+import madilyn from '../assets/boroughs/madilyn.png'
+import holloway from '../assets/boroughs/holloway.gif'
+import caroline from '../assets/boroughs/caroline.png'
+import lower from '../assets/boroughs/lower.png'
 
 const Boroughs = () => {
   const [currentProfile, setCurrentProfile] = useState(0);
@@ -58,26 +62,39 @@ const Boroughs = () => {
   const profiles = [
 
     {
-
-      name: "Anne-hatta",
+      name: "Midnight Anne-hatta",
       image: city1,
-      description: "The flashy and expensive Borough of dreams. No crisis ever could make anyone want to leave this place. Mors tested this hypothesis using independent research. Only 5% of this place remains recognizable. This place died so hard, the literal city went to heaven.",
+      description: "Flashy and expensive land of dreams, the sun never reaches this part of Anne-hatta. Nothing ever could make anyone want to leave this place. Mors tested this hypothesis using independent research. Only 5% of this place remains recognizable. \"This place died so hard, it went to heaven.\" - The BiWeekly",
       /*Midnight Enhatta, Anne-hatta, moon */
       /* Every ambitious young adult wishes they could move here because it is objectively the only way they could ever pursue their dreams.*/
     },
     {
-      name: "North Madilyn",
-      image: city1,
-      description: "Gained popularity after Moon was deemed \"a bad idea\" by The BiWeekly. Too much music and an excess of hipsters, Mors has been avoiding this area. This place is currently deemed, \"Not a bad idea\" by the BiWeekly.",
+      name: "Upper Madilyn",
+      image: madilyn,
+      description: "Gained popularity after Moon was deemed \"a bad idea\" by The BiWeekly. Overstimulating music and an excess of hipsters, Mors has been avoiding this area. This place is currently deemed, \"Not a bad idea\" by the BiWeekly.",
 
     },
+
+    {
+      name: "Miss Holloway",
+      image: holloway,
+      description: "The ring around the planet, where the angels come from. It is believed that heaven is situated someplace along it, and that when people die, they find themselves here, until a train picks them up and drops them someplace warm. The angels have neither denied nor accepted this claim. No one even remembers the origins of this rumor",
+
+    },
+
     {
       name: "Caroline",
-      image: city1,
-      description: "Once riddled with crime, now sees a record breaking zero percent crime rates. Now its just a culturally rich borough full of parks. Mors loves spending his sundays here.",
+      image: caroline,
+      description: "Nestled in gardens and parks, Mors had a hard time spotting and unpeopling people. Afraid that his average kills per second would drop, he took off to more populated city spaces. Our protagonist lives here.",
+    },
+
+    {
+      name: "Lower Anne-hatta",
+      image: lower,
+      description: "",
 
     },
-    
+
 
   ];
   const slideOffset = -currentProfile * slideWidth.current;
@@ -129,4 +146,33 @@ export default Boroughs;
     description: "Even Mors said no. This place will be blocked off because we don't want players to accidentally wander here. Please don't accidentally wander here.",
 
   },
+
+======================================
+----
+draft
+----
+flashy city
+desert
+submerged city
+snow
+hell? (no, hell doesn't exist.)
+mountains.
+forests
+island/beach
+family friendly park place - 
+------
+====================================
+
+====================================
+--------------
+Definitely there
+---------------
+flashy city    - Midtime Anne-Hatta     ( midtown manhattan)           
+sth            - Miss (you keep losing it) holloway       (red band has heaven, sometimes it comes close to the surface. a train goes around it to drop you to heaven?)
+ffpp           - caroline       (Carrol gardens)
+edgy art city  - Upper Madilyn  (North Brooklyn (Williamsburg))
+nice home      - downtime Anne-Hatta	 	(tribeca)
+---------------
+====================================
+
 */
--- src/pages/Characters.js
+++ src/pages/Characters.js
@@ -1,12 +1,19 @@
+import fox from "../assets/characters/fox.png"
+import mors from "../assets/characters/mors.png"
+import player from "../assets/characters/player.png"
+import yeka from "../assets/characters/yeka.png"
+
 const Characters = () => {
 
 
 
     return (
-        <div style={{display:"flex",justifyContent:"center" , alignItems:"center"}}>
-            <div className="card-container" style={{alignItems:"center"}} >
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+            <div className="card-container" style={{ alignItems: "center" }} >
 
-                <div className="card">Under construction.</div>
+                <div className="card">
+                    Under construction.
+                </div>
             </div>
         </div>
 
