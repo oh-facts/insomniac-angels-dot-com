d802b1a4f3705edb08d505369ddc34aa8849608a
oh-facts <as17793@nyu.edu>
2023-08-03 12:38:17 +0530

    updated error404 page

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,13 +1,18 @@
+import molly from "../assets/team/molly.png"
 
 import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
+        <div style={{ display:"flex", alignItems:"center",flexDirection:"column"}}>
             <br />
-         
+
             <h1>404 - Page Not Found</h1>
             <br />
+            <br />
+
+            <img src={molly} style={{ width: "20rem"}} />
+            
             <br />
             <p>This page doesn't exist. How did you come here. Why did you come here. And when will you leave.</p>
             <br />
@@ -16,6 +21,8 @@ const NotFound = () => {
             <br />
             <br />
             if that's why you're here, I forgive you.
+            <br />
+            <br />
         </div>
     );
 };
